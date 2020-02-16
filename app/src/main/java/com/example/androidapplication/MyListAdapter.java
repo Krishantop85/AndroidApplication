306cad346485cf40c575a3b83eea445b6da9a6f4
Krishan <krishan.kumar1@hashedin.com>
2020-02-17 03:17:13 +0530

    initial commit.

---
+++ app/src/main/java/com/example/androidapplication/MyListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.androidapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
+
+    private MyListData[] listData;
+
+    // RecyclerView recyclerView;
+    public MyListAdapter(MyListData[] listdata) {
+        this.listData = listdata;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem = layoutInflater.inflate(R.layout.list_tems, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        final MyListData myListData = listData[position];
+        holder.textView.setText(listData[position].getDescription());
+        holder.imageView.setImageResource(listData[position].getImgId());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"click on item: "+myListData.getDescription(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.imageView = itemView.findViewById(R.id.imageView);
+            this.textView = itemView.findViewById(R.id.textView);
+            relativeLayout = itemView.findViewById(R.id.relativeLayout);
+        }
+    }
+}
